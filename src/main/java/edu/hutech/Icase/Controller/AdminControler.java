4234c232f4e3daa20f3610eca5b865bce76b57a2
AnhDuc <ducle6487@gmail.com>
2021-05-16 20:17:13 +0700

    admin login page

---
+++ src/main/java/edu/hutech/Icase/Controller/AdminControler.java
@@ -0,0 +1,28 @@
+package edu.hutech.Icase.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.hutech.Icase.Model.AdminModel;
+
+@Controller
+public class AdminControler {
+
+	@GetMapping("/admin/login")
+	public String adminLogin() {
+		return "login";
+	}
+
+	@PostMapping(path = "checkLogin")
+	public String checkLogin(AdminModel admin) {
+
+		if (admin.getUserName().equals("admin") && admin.getPassword().equals("anhducle")) {
+			return "redirect:/home";
+		} else {
+			return "redirect:/admin/login";
+		}
+
+	}
+
+}
